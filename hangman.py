2e24277604cee774d67c5e1b7cd1865542cac0c4
0x2V <0x2V369@gmail.com>
2024-07-30 20:20:32 +0300

    Added new functions for clearer code

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # Hangman pictures for UI
 HANGMAN_PICTURES = ['''
@@ -57,57 +58,56 @@ WORDS = ['rainbow', 'computer', 'science', 'programming',
          'reverse', 'water', 'board', 'geeks', 'coconut', 'enlightened',
          'snowstorm', 'python', 'solidity']
 
-def check_letter_in_word(user_input):
-    for index in range(0, len(chosen_word)):
+def choose_word(words):
+    return random.choice(words)
+
+
+def display_current_progress(letter_guessed_right):
+    print("Current progress : ", " ".join(letters_guessed_right))
+    print("\n")
+
+
+def check_letter_in_word(chosen_word, user_input, letters_guessed_right):
+    for index in range(len(chosen_word)):
         if chosen_word[index] == user_input:
             letters_guessed_right[index] = user_input
-        else:
-            continue
 
 
-# Welcome prompt
 print("Welcome to the game of Hangman, I wish you good luck")
 
-
-
 lives_used = 0
-letters_guessed_right = []
+chosen_word = choose_word(WORDS)
+letters_guessed_right = ["_" for _ in range(len(chosen_word))]
 letters_guessed_wrong = []
-chosen_word = random.choice(WORDS)
-print(f"chosen word: {chosen_word}")
 
-for line in range(len(chosen_word)):
-    letters_guessed_right.append("_")
 
 print("This is the word that you gotta guess:")
-
-for i in letters_guessed_right:
-    print(i, end=" ")
-print("\n")
+display_current_progress(letters_guessed_right)
 
 while lives_used < 6:
     print(HANGMAN_PICTURES[lives_used])
+    display_current_progress(letters_guessed_right)
     guess = str(input("Guess a letter: ")).casefold()
 
-    # Check if guessed letter is in the word
-    if guess in letters_guessed_wrong:
-        print("\nYou have already tried this letter!")
-    elif guess in chosen_word:
-        # Update the letters_guessed
-        check_letter_in_word(guess)
-        letters_guessed_wrong.append(guess)
-        for i in letters_guessed_right:
-            print(i, end=" ")
-        print()
+    if len(guess) != 1 or not guess.isalpha():
+        print("\nPlease enter single letter!")
+        continue
 
-        # Check if player has guessed all the letters
-        if "_" not in letters_guessed_right:
-            print("\nCongratulations, you have guessed the word!")
-            break
+    if guess in letters_guessed_wrong or guess in letters_guessed_right:
+        print(f"\nYou have already tried '{guess}'!")
+        continue
+
+    if guess in chosen_word:
+        check_letter_in_word(chosen_word, guess, letters_guessed_right)
     else:
         print(f"Letter '{guess}' is not in the word!")
         letters_guessed_wrong.append(guess)
         lives_used += 1
+
+    if "_" not in letters_guessed_right:
+        display_current_progress(letters_guessed_right)
+        print("\nCongratulations, you have guessed the word!")
+        break
 
 if lives_used == 6:
     print(HANGMAN_PICTURES[6])
